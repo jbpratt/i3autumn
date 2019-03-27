b2470c861c0b0bec2657f69a51ac31ad39903f17
jbpratt78 <jbpratt78@gmail.com>
2019-03-27 07:25:25 -0500

    reworked copy func

--- cmd/backup.go
+++ cmd/backup.go
@@ -4,7 +4,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"time"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
@@ -14,16 +13,22 @@ var backupCmd = &cobra.Command{
 	Use:   "backup",
 	Short: "adds a task to your task list",
 	Run: func(cmd *cobra.Command, args []string) {
-		dt := time.Now()
-		d := dt.Format("01-02-2006")
+		//dt := time.Now()
+		//d := dt.Format("01-02-2006")
 		home, err := homedir.Dir()
 		check(err)
 		makePackageDirectories(home)
-		err = copyXresources(home, d)
+		err = copyFile(home+"/.Xresources", home+"/.i3/autumn/old/.Xresources")
+		//err = copyXresources(home, d)
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = copyi3Config(home, d)
+		err = copyFile(home+"/.i3/config", home+"/.i3/autumn/old/.i3config")
+		//err = copyi3Config(home, d)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = copyFile(home+"/.i3/status", home+"/.i3/autumn/old/.i3status")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -32,36 +37,37 @@ var backupCmd = &cobra.Command{
 
 func makePackageDirectories(home string) error {
 	log.Println("Checking if temp directories already exist")
-	if _, err := os.Stat("tmp"); os.IsNotExist(err) {
-		log.Println("Making new temporary directory...")
-		err := os.MkdirAll("tmp/old/", os.ModePerm)
+	if _, err := os.Stat(home + "/.i3/autumn"); os.IsNotExist(err) {
+		log.Println("Making new directory...")
+		err := os.MkdirAll(home+"/.i3/autumn/new", os.ModePerm)
 		if err != nil {
 			return err
 		}
-		err = os.Mkdir("tmp/new/", os.ModePerm)
+		err = os.Mkdir(home+"/.i3/autumn/old", os.ModePerm)
 		if err != nil {
 			return err
 		}
-		log.Println("tmp/{new,old} was made...")
+		log.Println("~/.i3/autumn/{new,old} was made...")
+		return nil
 	}
-	log.Println("Temporary directory already exists")
+	// check if {new,old} already exist, make per date dir in old
+	log.Println("~/.i3/autumn directory already exists")
 	return nil
 }
 
-// consolidate these copy funcs to one
-func copyXresources(path, date string) error {
-	log.Println("Attempting to copy ~/.Xresources into tmp/old...")
-	if _, err := os.Stat(path + "/.Xresources"); os.IsNotExist(err) {
+// takes in path (/home/x/.i3/config) copys to newLocation (/home/x/.i3/autumn/old/.i3config)
+func copyFile(path, newLocation string) error {
+	log.Println("Attempting to copy " + path + " to " + newLocation)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return err
 	}
-	log.Println(".Xresources was found...")
-	src, err := os.Open(path + "/.Xresources")
+	log.Println(path + " was found...")
+	src, err := os.Open(path)
 	if err != nil {
-		return err
+		return nil
 	}
 	defer src.Close()
-	log.Println("Found file, attempting to copy...")
-	dst, err := os.Create("tmp/old/" + time.Now().String() + ".Xresources")
+	dst, err := os.Create(newLocation)
 	if err != nil {
 		return err
 	}
@@ -74,40 +80,7 @@ func copyXresources(path, date string) error {
 	if err != nil {
 		return err
 	}
-	log.Println("Successfully copied .Xresources to the temporary directory")
-	return nil
-}
-
-func copyi3Config(path, date string) error {
-	log.Println("Attempting to copy ~/.i3/config into tmp/old...")
-	if _, err := os.Stat(path + "/.i3/config"); os.IsNotExist(err) {
-		return err
-	}
-	log.Println(".i3/config was found...")
-	src, err := os.Open(path + "/.i3/config")
-	if err != nil {
-		return err
-	}
-	defer src.Close()
-	log.Println("Found file, attempting to copy...")
-	dst, err := os.Create("tmp/old/" + time.Now().String() + ".i3config")
-	if err != nil {
-		return err
-	}
-	defer dst.Close()
-	_, err = io.Copy(dst, src)
-	if err != nil {
-		return err
-	}
-	err = dst.Sync()
-	if err != nil {
-		return err
-	}
-	log.Println("Successfully copied .i3/config to the temporary directory")
-	return nil
-}
-
-func copyi3StatusConf(path, date string) error {
+	log.Println("Success...")
 	return nil
 }
 
